1f0eb5d051043a6b2718d07738afde7d44933d39
Fadzri Priananda <fadzripriananda@Fadzris-MacBook-Air.local>
2019-11-19 21:16:01 +0700

    Add insert tech stacks

--- controller/techStacks.js
+++ controller/techStacks.js
@@ -1,9 +1,30 @@
+const shortid = require('shortid')
+
 let techStacks = {}
 
 techStacks.getAll = function(req, res) {
     const result = req.db.get('techStacks').value()
     console.log('RESULT', JSON.stringify(result))
     res.send(result)
+}
+
+techStacks.insert = function(req,res) {
+    const data = {
+        id: shortid.generate(),
+        title: req.body.title,
+        description: req.body.description,
+        url: req.body.url
+    }
+    const result = req.db
+    .get('techStacks')
+    .push(data)
+    .write()
+    if (result) {
+        console.log('RESULT', JSON.stringify(data))
+        res.send(data)
+    } else {
+        res.status(500).send('General Error')
+    }
 }
 
 module.exports = techStacks
--- routes.js
+++ routes.js
@@ -3,7 +3,9 @@ const express = require('express')
 const techStacksController = require('./controller/techStacks')
 
 const Router = express.Router()
+
 Router.get('/tech-stacks', techStacksController.getAll)
+Router.post('/tech-stacks', techStacksController.insert)
 
 
 module.exports = Router
